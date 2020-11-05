323512abf1a30ffc28844602ba7d58dfef55ac20
konrada111 <73497549+konrada111@users.noreply.github.com>
2020-11-05 13:47:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 var map = L.map('map',{
-    center: [43.64701, -79.39425],
+    center: [52.7367900, 15.2287800],
     zoom: 15
     });
+
+    L.marker([52.7367900, 15.2287800]).addTo(map);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+
+    function addCityFunction() {
+        var node = document.createElement("LI");
+        node.setAttribute('class','list-group-item');
+        var textnode = document.createElement("Label")
+        textnode.innerHTML = document.getElementById("addCityInput").value
+        node.appendChild(textnode);
+        document.getElementById("cityList").appendChild(node);
+      }
